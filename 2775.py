9835723b72d1e375618ba5780fa8a0216f85654d
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2021-10-02 19:07:09 +0900

    2775

--- 2775.py
+++ 2775.py
@@ -1,2 +1,12 @@
 #부녀회장이 될테야
 
+n = int(input())
+
+for _ in range(n):
+    k = int(input())
+    n = int(input())
+    f0 = [x for x in range(1, n+1)]
+    for k in range(k):
+        for i in range(1, n):
+            f0[i] += f0[i-1]
+    print(f0[-1])
