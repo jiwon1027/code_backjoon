6e6c0c754cb5bd8ac97af320aa9dfdaf44a2200a
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2021-11-21 21:46:47 +0900

    21.11.21

--- 15650.py
+++ 15650.py
@@ -1,16 +1,16 @@
-def DFS():
+def DFS(j):
     if len(s) == M:
         print(*s)
         return
 
-    for i in range(1, N + 1):
+    for i in range(j, N + 1):
         if i in s: #가지치기
             continue
 
         s.append(i)
-        DFS()  # 함수 다시 호출
+        DFS(i+1)  # 함수 다시 호출
         s.pop()  # 원상복귀
 
 N, M = map(int, input().split())
 s = []  # 출력 수열 넣을 stack
-DFS()
+DFS(1)
