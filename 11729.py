fa83ade8cac2b7b988bc5ae42a7474da7664322c
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2021-12-07 18:29:26 +0900

    21.12.07

---
+++ 11729.py
@@ -0,0 +1,15 @@
+data = []
+def hanoi(N, start, to, via):
+    if N == 1:
+        data.append([start,to])
+    else:
+        hanoi(N-1, start, via, to)
+        data.append([start,to])
+        hanoi(N-1, via, to, start)
+
+n = int(input())
+hanoi(n, '1', '3', '2')
+
+print(len(data))
+for i in data:
+    print(' '.join(i))
--- test.py
+++ test.py
@@ -1,14 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x_sin = np.arange(0,2*np.pi*6,0.1)
-y_sin = x_sin * np.sin(x_sin)
 
-x_cos = np.arange(0,2*np.pi*6,0.1)
-y_cos = 20 * np.cos(x_cos)
-
-
-
-plt.plot(x_sin,y_sin,'r-')
-plt.plot(x_cos,y_cos, 'b--')
 
 plt.show()
