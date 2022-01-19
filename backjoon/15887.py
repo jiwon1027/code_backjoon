87267ac38173f6d08683e6e13f57f4ed1baa4f89
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2022-01-19 22:01:57 +0900

    22.01.19

--- backjoon/15887.py
+++ backjoon/15887.py
@@ -1,1 +1,21 @@
 n = int(input())
+data = list(map(int,input().split()))
+
+import copy
+data_temp = copy.deepcopy(data)
+data_temp.sort()
+result = []
+i = 1
+cnt = 0
+
+while data != data_temp:
+    if data[i-1] != i:
+        cnt += 1
+        k = data.index(i) + 1
+        data[i-1:data.index(i)+1] = data[i-1:data.index(i)+1][::-1]
+        result.append([i,k])
+    i += 1
+print(cnt)
+
+for i in result:
+    print(*i)
---
+++ codeup/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+
+float x;
+scanf("%f",&x);
+printf("%.2f\n",x);
+
+return 0;
+}
