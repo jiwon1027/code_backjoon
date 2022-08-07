d1ce346c311322aad864e8f970d4cb12da2f0536
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2022-08-07 14:40:09 +0900

    [Gold V] Title: 탑, Time: 752 ms, Memory: 87200 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws  IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        Stack<int[]> stack = new Stack<>();
+        
+        int size = Integer.parseInt(br.readLine());
+       
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        
+        for(int i = 1; i < size+1; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty()) {
+                if(stack.peek()[1] >= num) {
+                    sb.append(stack.peek()[0]).append(" ");
+                    break;
+                }
+                stack.pop();
+            }
+            
+            if(stack.isEmpty()) 
+                sb.append("0 ");
+            
+            stack.push(new int[] {i, num});
+        }
+        System.out.print(sb);
+
+	}
+}
