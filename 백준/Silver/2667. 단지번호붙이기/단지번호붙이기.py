9997783db8ea61af595c3a197f46e53e5827ec74
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2022-08-14 10:55:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 31104 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,38 @@
+def dfs(x,y):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    global res
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    if board[x][y] == 1:
+        res += 1
+        board[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            dfs(nx,ny)
+        return 1
+    return 0
+
+
+
+n = int(input())
+
+board = []
+data = []
+for _ in range(n):
+    board.append(list(map(int,input())))
+
+res = 0
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            if dfs(i,j) == 1:
+                data.append(res)
+                res = 0
+
+print(len(data))
+data.sort()
+for i in data:
+    print(i)
