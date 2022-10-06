34349039d3cb95fb1c5d91096757d1de079d0615
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2022-10-06 19:46:01 +0900

    [D5] Title: 오! 나의 여신님, Time: 116 ms, Memory: 25,136 KB -BaekjoonHub

--- SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
+++ SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
@@ -1,89 +1,117 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static class Info{
-		int x,y;
-		public Info(int y, int x) {
-			this.x=x;
-			this.y=y;
-		}
-	}
-	static char arr[][];
-	static int check[][];
-	static int row,col;
-	final static int dx[]= {0,1,0,-1};
-	final static int dy[]= {-1,0,1,0};
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int test = Integer.parseInt(br.readLine().trim());
-		for(int t=1;t<=test;t++) {
-			String s = br.readLine();
-			StringTokenizer st = new StringTokenizer(s);
-			row =Integer.parseInt(st.nextToken());
-			col =Integer.parseInt(st.nextToken());
-			//초기화
-			arr=new char[row][col];
-			check=new int[row][col];
-			Queue<Info> evil = new LinkedList<>();
-			Queue<Info> q = new LinkedList<>();
-			
-			for(int i=0;i<row;i++) {
-				String ss = br.readLine().trim();
-				arr[i]=ss.toCharArray();
-				for(int j=0;j<ss.length();j++) {
-					if(arr[i][j]=='*') 		evil.offer(new Info(i,j));					
-					else if(arr[i][j]=='S')			q.offer(new Info(i,j));
-				}
-			}
-			boolean finish=false;
-			int result=0;
-			Info ii;
-			while(true) {
-				//악마 이동
-				int len = evil.size();
-				for(int i=0;i<len;i++) {
-					ii=evil.poll();
-					int cx = ii.x;
-					int cy = ii.y;
-					for(int j=0;j<4;j++) {
-						int nx = cx + dx[j];
-						int ny = cy + dy[j];
-						if(nx>=0 && ny>=0 && nx<col && ny<row && (arr[ny][nx]=='.' || arr[ny][nx]=='S')) {
-							arr[ny][nx]='*';
-							evil.offer(new Info(ny,nx));
-						}
-					}
-				}
-				//수연이 이동
-				len = q.size();
-				for(int i=0;i<len;i++) {
-					ii=q.poll();
-					int cx = ii.x;
-					int cy = ii.y;
-					if(arr[cy][cx]=='D') {
-						finish=true;
-						break;
-					}
-					for(int j=0;j<4;j++) {
-						int nx = cx + dx[j];
-						int ny = cy + dy[j];
-						if(nx>=0 && ny>=0 && nx<col && ny<row && (arr[ny][nx]=='.' || arr[ny][nx]=='D')) {
-							if(arr[ny][nx]!='D')
-								arr[ny][nx]='S';
-							q.offer(new Info(ny,nx));
-						}
-					}
-				}
-				if(finish || q.isEmpty()) break;				
-				result++;
-			}
-			if(!finish) System.out.println("#"+t+" "+"GAME OVER");
-			else System.out.println("#"+t+" "+result);
-		}
-	}
+    static int N,M;
+    static char[][] board;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+
+    static Queue<int[]> devilQueue;
+    static Queue<int[]> personQueue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int tc = 1; tc < T+1; tc++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            devilQueue = new ArrayDeque<>();
+            personQueue = new ArrayDeque<>();
+            board = new char[N][M];
+
+            for (int i = 0; i < N; i++) {
+                String input = br.readLine();
+                for (int j = 0; j < M; j++) {
+                    board[i][j] = input.charAt(j);
+                    if (board[i][j] == 'S'){
+                        personQueue.add(new int[]{i,j,0});
+                    }
+                    else if (board[i][j] == '*'){
+                        devilQueue.add(new int[]{i,j});
+                    }
+                }
+            }
+
+            int res = bfs();
+
+            if (res == -1)
+                System.out.println("#"+tc+" GAME OVER");
+            else
+                System.out.println("#"+tc+" "+res);
+
+
+        }
+    }
+
+    public static int bfs(){
+        while (!personQueue.isEmpty()){
+            //악마 이동
+            int length = devilQueue.size();
+            for (int i = 0; i < length; i++) {
+                int[] poll_data = devilQueue.poll();
+                int x = poll_data[0];
+                int y = poll_data[1];
+                for (int j = 0; j < 4; j++) {
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+                    if (nx<0 || ny<0 || nx>=N || ny>=M)
+                        continue;
+                    if (board[nx][ny] == '.' || board[nx][ny] == 'S'){
+                        board[nx][ny] = '*';
+                        devilQueue.add(new int[]{nx,ny});
+                    }
+                }
+            }
+
+            //사람 이동
+            length = personQueue.size();
+            for (int i = 0; i < length; i++) {
+                int[] poll_data = personQueue.poll();
+                int x = poll_data[0];
+                int y = poll_data[1];
+                int cnt = poll_data[2];
+
+                for (int j = 0; j < 4; j++) {
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+
+                    if (nx<0 || ny<0 || nx>=N || ny>=M)
+                        continue;
+
+                    if (board[nx][ny] == 'D')
+                        return cnt+1;
+
+
+                    if (board[nx][ny] == '.'){
+                        board[nx][ny] = 'S';
+                        personQueue.add(new int[]{nx,ny,cnt+1});
+                    }
+                }
+            }
+        }
+       return -1;
+    }
+
+
+    public static void print(){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+
 }
