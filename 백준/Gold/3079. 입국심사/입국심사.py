42b00ad745303e3cd6bd94f7857926e2fdbe9ac1
Ji won Lee <68945145+jiwon1027@users.noreply.github.com>
2023-02-08 21:34:12 +0900

    [Gold V] Title: 입국심사, Time: 1048 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -1,14 +1,22 @@
+'''
+그니까 각각 심사대가 있는데 거기로 사람을 배정해서 가장 짧게(최소)로 이용하는 시간은 얼마인지 구하는 것
+
+각각 심사대에 따라서 하나씩 배정해야하는데 N,M 범위보니까 일일히 하는건 아닌 것 같음
+범위 사이즈보니까 이분탐색이긴하네
+어떤걸 기준으로 이분탐색해야되나? ==> 시간
+
+'''
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-time = sorted(list(int(input()) for _ in range(N)))
+N, M = list(map(int, input().split()))
+time = list(int(input()) for _ in range(N))
 
 start = 0
-end = min(time) * M
+end = max(time) * M
 result = 0
 
-while start <= end :
+while start <= end:
     mid = (start + end) // 2
     temp = 0
     for t in time:
@@ -16,8 +24,8 @@ while start <= end :
 
     if temp >= M:
         end = mid - 1
-        result = mid #sorted했기 때문에 min()해줄 필요 x
+        result = mid
     else:
         start = mid + 1
-    #print(start,end)
+
 print(result)
